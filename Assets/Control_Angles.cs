1c1849b7c1b5a62ad9b8ef3243b177361e1d6510
rsl18 <rsluo@zonemail.clpccd.edu>
2016-12-07 02:00:50 -0800

    Revert "init"
    This reverts commit 8f7d72b04e8aa0a11115419c12157362969d7b16.

--- Assets/Control_Angles.cs
+++ Assets/Control_Angles.cs
@@ -1,117 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
 public class Control_Angles : MonoBehaviour {
 
-    public GameObject left_lever;
-    public GameObject right_lever;
-    public GameObject hori_crank;
-    public GameObject vert_crank;
+	public GameObject left_lever;
+	public GameObject right_lever;
+	public GameObject hori_crank;
+	public GameObject vert_crank;
 
-    HingeJoint left_lever_joint;
-    HingeJoint right_lever_joint;
-    HingeJoint hori_crank_joint;
-    HingeJoint vert_crank_joint;
+	HingeJoint left_lever_joint;
+	HingeJoint right_lever_joint;
+	HingeJoint hori_crank_joint;
+	HingeJoint vert_crank_joint;
 
-    Transform left_lever_transform;
-    Transform right_lever_transform;
-    Transform hori_crank_transform;
-    Transform vert_crank_transform;
+	Transform left_lever_transform;
+	Transform right_lever_transform;
+	Transform hori_crank_transform;
+	Transform vert_crank_transform;
 
-    float vert_lower = 360;
-    float vert_upper = -1800;
+	void Start() { 
+		left_lever_joint = left_lever.GetComponent<HingeJoint> ();
+		right_lever_joint = right_lever.GetComponent<HingeJoint> ();
+		hori_crank_joint = hori_crank.GetComponent<HingeJoint> ();
+		vert_crank_joint = vert_crank.GetComponent<HingeJoint> ();
 
-    float vert_degree;
-    float vert_prev;
+		left_lever_transform = left_lever.GetComponent<Transform> ();
+		right_lever_transform = right_lever.GetComponent<Transform> ();
+		hori_crank_transform = hori_crank.GetComponent<Transform> ();
+		vert_crank_transform = vert_crank.GetComponent<Transform> ();
+	}
 
-    float vert_change;
+	/*void Update() {
+		Debug.Log (GetLeftCrankAngle ());
+		Debug.Log (GetRightCrankAngle ());
+		Debug.Log (GetLeftLeverAngle ());
+		Debug.Log (GetRightLeverAngle ());
+	}*/
 
+	public float GetLeftLeverAngle() {
+		return left_lever_joint.angle;
+	}
 
-    void Start() {
-        left_lever_joint = left_lever.GetComponent<HingeJoint>();
-        right_lever_joint = right_lever.GetComponent<HingeJoint>();
-        hori_crank_joint = hori_crank.GetComponent<HingeJoint>();
-        vert_crank_joint = vert_crank.GetComponent<HingeJoint>();
+	public float GetRightLeverAngle() {
+		return right_lever_joint.angle;
+	}
 
-        left_lever_transform = left_lever.GetComponent<Transform>();
-        right_lever_transform = right_lever.GetComponent<Transform>();
-        hori_crank_transform = hori_crank.GetComponent<Transform>();
-        vert_crank_transform = vert_crank.GetComponent<Transform>();
+	public float GetLeftCrankAngle() {
+		return hori_crank_joint.angle;
+	}
 
-        vert_degree = vert_crank_joint.angle;
-        vert_prev = vert_degree;
-    }
+	public float GetRightCrankAngle() {
+		return vert_crank_joint.angle;
+	}
 
-    void Update() {
+	public void SetLeftLeverAngle(float localX) {
+		left_lever_transform.localEulerAngles = new Vector3 (localX, left_lever_transform.localEulerAngles.y, left_lever_transform.localEulerAngles.z);
+	}
 
-        float curr = vert_crank_joint.angle;
-        if (curr < 0) {
-            curr = vert_lower + curr;
-        }
+	public void SetRightLeverAngle(float localX) {
+		right_lever_transform.localEulerAngles = new Vector3 (localX, right_lever_transform.localEulerAngles.y, right_lever_transform.localEulerAngles.z);
+	}
 
-        vert_change = Mathf.DeltaAngle(vert_prev, curr); 
+	public void SetLeftCrankAngle(float localX) {
+		hori_crank_transform.localEulerAngles = new Vector3 (localX, hori_crank_transform.localEulerAngles.y, hori_crank_transform.localEulerAngles.z);
+	}
 
-        vert_prev = curr;
-
-        vert_degree += vert_change;
-
-        if (vert_degree >= vert_lower) {
-            if (vert_change > 0) {
-                vert_degree = vert_lower;
-                vert_change = 0;
-                return;
-            }
-        } else if (vert_degree <= vert_upper) {
-            if (vert_change < 0) {
-                vert_degree = vert_upper;
-                vert_change = 0;
-                return;
-            }
-        }
-        //Debug.Log(vert_degree);
-
-
-    }
-
-    public float GetLeftLeverAngle() {
-        //Debug.Log("Left Lever Angle is " + left_lever_joint.angle);
-        return left_lever.transform.localRotation.eulerAngles.x;
-    }
-
-    public float GetRightLeverAngle() {
-        return right_lever.transform.localRotation.eulerAngles.x;
-    }
-
-    public float GetHorCrankAngle() {
-
-        return hori_crank.transform.localRotation.eulerAngles.x;
-    }
-
-    public float GetVertCrankAngle() {
-
-        return vert_degree;
-    }
-
-    public float GetVertCrankDelta() {
-        return vert_change;
-    }
-
-    public void SetLeftLeverAngle(float localX) {
-        left_lever_transform.localEulerAngles = new Vector3(localX, left_lever_transform.localEulerAngles.y, left_lever_transform.localEulerAngles.z);
-    }
-
-    public void SetRightLeverAngle(float localX) {
-        right_lever_transform.localEulerAngles = new Vector3(localX, right_lever_transform.localEulerAngles.y, right_lever_transform.localEulerAngles.z);
-    }
-
-    public void SetLeftCrankAngle(float localX) {
-        hori_crank_transform.localEulerAngles = new Vector3(localX, hori_crank_transform.localEulerAngles.y, hori_crank_transform.localEulerAngles.z);
-    }
-
-    public void SetRightCrankAngle(float localX) {
-        vert_crank_transform.localEulerAngles = new Vector3(localX, vert_crank_transform.localEulerAngles.y, vert_crank_transform.localEulerAngles.z);
-    }
+	public void SetRightCrankAngle(float localX) {
+		vert_crank_transform.localEulerAngles = new Vector3 (localX, vert_crank_transform.localEulerAngles.y, vert_crank_transform.localEulerAngles.z);
+	}
 
 
 }
